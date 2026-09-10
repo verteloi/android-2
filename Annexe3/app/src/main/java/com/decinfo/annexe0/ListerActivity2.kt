7701fe80092b29cb7d88b4cbf21d1ac3018490b1
eloi <eloivert@gmail.com>
2026-09-10 16:58:29 -0400

    annexe 3

--- Annexe3/app/src/main/java/com/decinfo/annexe0/ListerActivity2.kt
+++ Annexe3/app/src/main/java/com/decinfo/annexe0/ListerActivity2.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -24,9 +25,29 @@ class ListerActivity2 : AppCompatActivity() {
             insets
         }
         liste = findViewById(R.id.list)
+        liste.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, trierMemos())
 
         //liste.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,lireMemo())
 
     }
 
+    fun trierMemos() : ArrayList<String> {
+        val triee = ArrayList<String>()
+        var listeActuelle:ArrayList<Memo>? = null
+
+        try {
+            listeActuelle = SingletonSimle.recupérerList()
+            listeActuelle.sortBy{it.date}
+            // listeActuelle.forEach { it -> triee.add(it.memo) }
+
+            for (it in listeActuelle) {
+                triee.add(it.memo)
+            }
+        }
+        catch (f: FileNotFoundException) {
+            Toast.makeText(this, "pas de fichier", LENGTH_LONG). show()
+            finish();
+        }
+        return triee;
+    }
 }
--- Annexe3/app/src/main/java/com/decinfo/annexe0/SingletonSimle.kt
+++ Annexe3/app/src/main/java/com/decinfo/annexe0/SingletonSimle.kt
@@ -1,5 +1,12 @@
 package com.decinfo.annexe0
 
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+
 object SingletonSimle {
     var listMemo = ArrayList<Memo>()
 
@@ -8,5 +15,29 @@ object SingletonSimle {
     }
     fun ajouterList(memo: Memo){
         listMemo.add(memo)
+    }
+
+    fun serializerListe(context: Context) {
+        try {
+            val fos : FileOutputStream = context.openFileOutput("fichier.ser", MODE_PRIVATE)
+            val oos = ObjectOutputStream(fos) // buffer special pour écrire des objects and le flux de données binaire
+            oos.use {
+                oos.writeObject(listMemo)
+            }
+        } catch (e: Exception) {
+            println("probleme")
+        }
+    }
+
+    // recuperer la liste du fichier de la serialisation et retourner une copie de la liste
+    fun deserializerListe(context: Context) : ArrayList<Memo> {
+        if (listMemo.isEmpty()) { // pour rviter d'utiliser le fichier de serialisation sans en aboir vraiment besoin
+            val fis: FileInputStream = context.openFileInput("fichier.ser")
+            val ois = ObjectInputStream(fis)
+            ois.use {
+                listMemo = ois.readObject() as ArrayList<Memo>
+            }
+        }
+        return ArrayList(listMemo)
     }
 }
